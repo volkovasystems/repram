1d06f94374487871f9197318dd4c5da969532ca9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 15:58:36 +0800

    Updated test.

--- repram-test.js
+++ repram-test.js
@@ -1,9 +1,9 @@
 const assert = require( "assert" );
 const repram = require( "./repram.js" );
 
-assert.ok( repram( function( ){ return arguments; }, NUMBER, STRING )( "hello", 123, true ) );
+assert.ok( repram( function( ){ return arguments; }, NUMBER, STRING )( "hello", 123, true ), "should not throw error" );
 
 assert.equal( typeof repram( function( ){ return arguments; }, NUMBER, STRING )( "hello", 123, true ) == "object",
-				true, "should return true" );
+				true, "should be equal to 'object'" );
 
 console.log( "ok" );
